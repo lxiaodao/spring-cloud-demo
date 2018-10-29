3c9748169a90095efccb24b4df90eedb57629a62
yang <l_xiaodao@qq.com>
2018-10-29 15:13:59 +0800

    spring cloud网关集群示例代码

---
+++ gateway/src/main/java/gateway/ControllerFacade.java
@@ -0,0 +1,38 @@
+/*package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+
+@RestController
+@RibbonClient(name = "helloCluster", configuration = RibbonConfiguration.class)
+public class ControllerFacade {
+
+  @LoadBalanced
+  @Bean
+  RestTemplate restTemplate(){
+    return new RestTemplate();
+  }
+
+  @Autowired
+  RestTemplate restTemplate;
+
+  @RequestMapping("/hi")
+  public String hi(@RequestParam(value="name", defaultValue="Artaban") String name) {
+    String greeting = this.restTemplate.getForObject("http://helloCluster/greeting", String.class);
+    return String.format("%s, %s!", greeting, name);
+  }
+
+
+}
+
+*/
